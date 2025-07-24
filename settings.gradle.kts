6ded7f793bea2e9d14b8d1184314b483a6f719de
Ali Azaz Alam <ali.azaz.alam@hotmail.com>
2025-07-24 17:50:22 +0500

    refactor: Android 15 updates and dependency management
    This commit updates Gradle to the latest version, migrates to Kotlin DSL, and uses version catalogs for dependency management. It also includes configuration for publishing to GitHub Packages.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,14 +3,18 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 }
+
 rootProject.name = "QuickJS"
-include ':app', ':quickjs-android'
+include(":app", ":quickjs-android")
